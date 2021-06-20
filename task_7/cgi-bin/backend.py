b16ecc9760d6af37ccbd51622a8787aa017760ba
launchpad5682 <launchpad5682@gmail.com>
2021-06-20 21:56:30 +0530

    backend code added

---
+++ task_7/cgi-bin/backend.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import subprocess
+import cgi
+
+print('content-type: text/html')
+# allowing CORS because frontend is hosted on different server
+print('Access-Control-Allow-Origin: *')
+print()
+
+# get the values from the URL
+vars = cgi.FieldStorage()
+cmd = vars.getvalue('cmd')
+
+# saving and returning the output
+output = subprocess.getoutput("sudo "+cmd)
+
+print(output)
