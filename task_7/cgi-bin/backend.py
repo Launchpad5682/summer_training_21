f35105e1825c9cfd525847052048f5fe17461bc6
Saurabh Suthar <launchpad5682@gmail.com>
2021-06-24 12:39:18 +0530

    Comment for CORS

--- task_7/cgi-bin/backend.py
+++ task_7/cgi-bin/backend.py
@@ -5,7 +5,7 @@ import cgi
 
 print('content-type: text/html')
 # allowing CORS because frontend is hosted on different server
-print('Access-Control-Allow-Origin: *')
+print('Access-Control-Allow-Origin: *') # anyone can access or some defined IP addresses
 print()
 
 # get the values from the URL
